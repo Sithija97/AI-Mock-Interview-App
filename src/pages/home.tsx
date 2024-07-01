f223df53b72452e43b11fc66d03068f8518fb653
Sithija Shehara <nsithijashehara@gmail.com>
2024-07-01 23:17:44 +0530

    initial commit

--- src/pages/home.tsx
+++
@@ -1,8 +0,0 @@
-export const Home = () => {
-  return (
-    <div className="grid grid-cols-1 md:grid-cols-3 gap-2">
-      <div className="w-full md:col-span-2 bg-blue-200">table</div>
-      <div className="w-full bg-blue-200">form</div>
-    </div>
-  );
-};
