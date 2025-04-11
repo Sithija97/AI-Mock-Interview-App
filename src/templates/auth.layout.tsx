664fa1bbfca02ffeb12639c846788eeef62b9bff
Sithija Shehara <nsithijashehara@gmail.com>
2025-04-11 15:49:04 +0530

    UI improvement - auth layout

--- src/templates/auth.layout.tsx
+++ src/templates/auth.layout.tsx
@@ -3,6 +3,11 @@ import { Outlet } from "react-router-dom";
 export const AuthenticationLayout = () => {
   return (
     <div className="w-screen h-screen overflow-hidden flex items-center justify-center relative">
+      <img
+        src="/assets/img/bg.png"
+        className="absolute w-full h-full object-cover opacity-20"
+        alt=""
+      />
       <Outlet />
     </div>
   );
