902b812a0691f9b2bcd728b503199b6bdb72c08d
Sithija Shehara <nsithijashehara@gmail.com>
2025-04-21 23:54:06 +0530

    header comopnent structure

--- src/molecules/header.tsx
+++ src/molecules/header.tsx
@@ -1,3 +1,21 @@
+import { cn } from "@/lib/utils";
+import { Container } from "@/templates";
+import { useAuth } from "@clerk/clerk-react";
+
 export const Header = () => {
-  return <div>Header</div>;
+  const { userId } = useAuth();
+
+  return (
+    <header
+      className={cn("w-full border-b duration-150 transition-all ease-in-out")}
+    >
+      <Container>
+        <div>
+          {/* logo */}
+          {/* navigation */}
+          {/* profile */}
+        </div>
+      </Container>
+    </header>
+  );
 };
