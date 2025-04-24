d35af9c8ef94edd93f2e831ea02e4d32761fece2
Sithija Shehara <nsithijashehara@gmail.com>
2025-04-24 23:51:23 +0530

    headings component

---
+++ src/templates/dashboard.tsx
@@ -0,0 +1,14 @@
+import { Headings } from "@/molecules";
+
+export const Dashboard = () => {
+  return (
+    <div className="flex w-full items-center justify-between">
+      {/* headings */}
+      <Headings
+        title="Dashboard"
+        description="Create and start your AI Mock interview"
+      />
+      {/* content section */}
+    </div>
+  );
+};
